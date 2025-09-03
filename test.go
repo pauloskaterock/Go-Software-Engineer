e589db8320813fbddd1fc2c58fce5a2f1ba01c28
pauloskaterock <pauloengenharia5@yahoo.com>
2025-09-03 17:11:31 -0300

    pacotes em go, code academy course

--- test.go
+++ test.go
@@ -1,8 +1,16 @@
 package main
 
 import "fmt"
+import "strings"
+
+// func main() {
+//     fmt.Println("Olá, Mundo!")
+//     fmt.Println("Bem-vindo ao seu primeiro projeto em Go!")
+// }
+
 
 func main() {
     fmt.Println("Olá, Mundo!")
-    fmt.Println("Bem-vindo ao seu primeiro projeto em Go!")
+    // fmt.Println.ToUpper("Bem-vindo ao seu primeiro projeto em Go!")
 }
+
